ab2fb7d9e7b1a271032768a4f5457e0c0e474f94
Kaaveh <kaaveh@pm.me>
2020-10-13 14:46:14 +0330

    initial commit

---
+++ app/src/main/java/ir/massdev/youtibestatus/model/User.kt
@@ -0,0 +1,2 @@
+package ir.massdev.youtibestatus.model
+
---
+++ app/src/main/java/ir/massdev/youtibestatus/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.massdev.youtibestatus.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.massdev.youtibestatus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
