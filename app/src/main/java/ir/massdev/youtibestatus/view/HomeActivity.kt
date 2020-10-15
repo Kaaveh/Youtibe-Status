36b5ad95a1e27f7215ee9de94433883828edb27b
javad <j.shirkhani@clickhub.ir>
2020-10-15 21:07:26 +0330

    profile ui (Home activity) added

--- app/src/main/java/ir/massdev/youtibestatus/view/HomeActivity.kt
+++ app/src/main/java/ir/massdev/youtibestatus/view/HomeActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.massdev.youtibestatus.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+        //
+
     }
 }
