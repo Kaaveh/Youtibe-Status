739cc89e4e3c05f3e147b9c3cc9fe0fccf808859
javad <j.shirkhani@clickhub.ir>
2020-10-17 19:29:34 +0330

    AppBar layout added

--- app/src/main/java/ir/massdev/youtibestatus/view/HomeActivity.kt
+++ app/src/main/java/ir/massdev/youtibestatus/view/HomeActivity.kt
@@ -1,15 +1,24 @@
 package ir.massdev.youtibestatus.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ir.massdev.youtibestatus.R
 
+
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         //
+        toolbar = findViewById(R.id.too_bar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+/*        val w: Window = window
+        w.setTitle("My title")*/
 
     }
 }
